46749d5be86e0e076e4e2f65a997c7ba35b67e2b
Robert <boren534@gmail.com>
2025-09-30 12:15:50 +0200

    Skapade ToDoPanel
    Closes #9

--- src/Todo_application.java
+++ src/Todo_application.java
@@ -1,31 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
 
-    public class  Todo_application extends JFrame{
-      
-      private final JTextField input = new JTextField();
-      private final JButton addButton = new JButton("Lägg till");
+public class Todo_application extends JFrame {
 
+    private final JTextField input = new JTextField();
+    private final JButton addButton = new JButton("Lägg till");
+    private final JList<String> todoList = new JList<>();
 
-    public Todo_application () {
+    public Todo_application() {
         setTitle("ToDo Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout(12, 12));
-        ((JComponent)getContentPane()).setBorder(BorderFactory.createEmptyBorder(12,12,12,12));
-        
-        
+        ((JComponent) getContentPane()).setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+
         // Top Part: textbox and add button
-        JPanel topPart = new JPanel(new BorderLayout(8,8));
+        JPanel topPart = new JPanel(new BorderLayout(8, 8));
         input.setPreferredSize(new Dimension(10, 30));
         topPart.add(input, BorderLayout.CENTER);
         topPart.add(addButton, BorderLayout.EAST);
         add(topPart, BorderLayout.NORTH);
+
+        // Mitten: ToDo | pilar | Done
+        JPanel center = new JPanel(new GridBagLayout());
+        add(center, BorderLayout.CENTER);
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(6,6,6,6);
+        c.fill = GridBagConstraints.BOTH;
+        c.weighty = 1;
+        c.weightx = 1;
         
+
+        // Vänster panel (ToDo)
+        JPanel ToDoPanel = new JPanel(new BorderLayout(6, 6));
+        ToDoPanel.add(new JLabel("ToDo-lista"), BorderLayout.NORTH);
+        todoList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);// Lägger till markering
+        ToDoPanel.add(new JScrollPane(todoList), BorderLayout.CENTER);// Lägger till scrollpanel med listan i
+        c.gridx = 1; c.gridy = 0;
+        center.add(ToDoPanel, c);
+
+
         setVisible(true);
         setMinimumSize(new Dimension(720, 480));
         setLocationRelativeTo(null);
+
     }
-    
+
     public static void main(String[] args) throws Exception {
         new Todo_application();
 
